880d9ee84e30f943b07e9483e5c189e225ac8d65
yasser <yasser.0.yahia@protonmail.com>
2024-10-24 22:24:30 +0300

    task-3

--- src/index.ts
+++ src/index.ts
@@ -8,21 +8,35 @@ const port = process.env.PORT || 3000 ;
 
 app.use(cors())
 
+app.use(express.json())
 
 
-function sendJsonSuccess (req: Request , res:Response){
+function onlyPassAuthenticated (req: Request , res:Response, next:NextFunction){
+    const auth = req.headers["authorization"]
+    if (!auth){
+        res.status(401).json({message: "meeeeeeen?"})
+        return 
+    }
+    next()
+}
+
+
+const sendJsonSuccess = (req: Request , res:Response) =>{
     res.status(200).json({success : true})
 }
 
 app.get('/task1' , (req:Request , res:Response) =>{
     res.type("text/plain").status(200).send("hello world")
 })
-
 app.get('/task2', sendJsonSuccess)
 app.post('/task2', sendJsonSuccess)
 app.patch('/task2' , sendJsonSuccess)
 app.put('/task2' ,sendJsonSuccess )
 app.delete('/task2' ,sendJsonSuccess )
+
+
+app.get('/task3' ,onlyPassAuthenticated ,sendJsonSuccess)
+
 
 app.listen(port , ()=>{
     console.log("listening on port 3000....")
