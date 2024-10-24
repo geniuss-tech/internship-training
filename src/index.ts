1f1b6b8f16835211e974c31288e95055f827f814
nistafa <nistafa447102@gmail.com>
2024-10-24 21:58:57 +0300

    task2

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,15 @@ import cors from "cors";
 
 const app = express();
 const port = process.env.PORT || 3000;
+const sendJsonSuccess = (req: Request, res: Response) => {
+  res.status(200).setHeader('Content-Type', 'application/json').json({ success: true });
+};
 
 app.use(cors());
 app.get('/task1', (req: Request, res: Response) => {
     res.status(200).setHeader('Content-Type', 'text/plain').send('hello world');
   });
+  app.all('/task2',sendJsonSuccess)
 
   app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
