f4829cbbd036efc23dadec77c951eaa514322cb2
ahmed radwan <ahmed.radwan.0666@gmail.com>
2024-10-24 20:03:18 +0300

    task 2,3

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 
 const app = express();
@@ -6,10 +6,38 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
+//Middleware
+const onlyPassAuthenticated = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (req.headers.authorization) {
+    next();
+  } else {
+    res.status(401).json({ message: "meeeeeeen?" });
+  }
+};
+
+const sendJsonSuccess = async (req: Request, res: Response, data: any) => {
+  res.status(200).json({ success: true });
+};
+
+// Task 1
 app.get("/task1", async (req: Request, res: Response) => {
   res.setHeader("Content-Type", "text/plain");
   res.status(200).send("hello world");
 });
+
+// Task 2
+app.get("/task2", sendJsonSuccess);
+app.post("/task2", sendJsonSuccess);
+app.patch("/task2", sendJsonSuccess);
+app.put("/task2", sendJsonSuccess);
+app.delete("/task2", sendJsonSuccess);
+
+// Task 3
+app.get("/task3", onlyPassAuthenticated, sendJsonSuccess);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
