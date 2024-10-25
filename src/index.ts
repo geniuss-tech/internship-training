95f157e031459068aa58878bd8df4288470165b0
baraaml <baraalearnsml@gmail.com>
2024-10-25 11:22:25 +0300

    made 5 endpoints and installed nodemon

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 const app = express();
 const port = process.env.PORT || 3000;
 
+
 app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
@@ -13,6 +14,17 @@ app.get('/', (req: Request, res: Response) => {
 app.get('/task1/', (req, res) => {
     res.status(200).contentType('text/plain').send('hello world');
   });
+
+  // Create 5 endpoints: GET, POST, PATCH, PUT, DELETE on the same path /task2
+  function sendJsonSuccess(req: Request, res: Response) {
+    res.status(200).json({ success: true });
+  }
+  
+app.get('/task2/', sendJsonSuccess);
+app.post('/task2/', sendJsonSuccess);
+app.patch('/task2/', sendJsonSuccess);
+app.put('/task2/', sendJsonSuccess);
+app.delete('/task2/', sendJsonSuccess);
 
   app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
