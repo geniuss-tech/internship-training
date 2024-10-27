6b7e95ddd96d99f8a4390b9167b7d031a75ae9df
Seif-salah <seif.salah3000@gmail.com>
2024-10-27 04:28:50 +0300

    t2

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,27 @@ const port = 3000;
 
 app.use(cors());
 
+// task one Begin
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
 app.get("/task1", (req: Request, res: Response) => {
   res.setHeader("Content-Type", "text/plain");
   res.status(200).send("hello world");
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+//Task one end
+
+// Task two Begin
+const sendJsonSuccess = function (req: Request, res: Response) {
+  res.status(200).json({ success: true });
+};
+
+app.get("/task2", sendJsonSuccess);
+app.post("/task2", sendJsonSuccess);
+app.patch("/task2", sendJsonSuccess);
+app.delete("/task2", sendJsonSuccess);
+app.put("/task2", sendJsonSuccess);
+
+// task Two End
