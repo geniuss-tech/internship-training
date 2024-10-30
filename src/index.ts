f5a2299f66493aa7545394ebd6f74a41021ecc11
Mariam_Elsioffy <mariam.sioffy@gmail.com>
2024-10-30 16:42:06 +0300

    task2 was added

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,26 @@ app.get('/task1', (req: Request, res: Response) => {
   res.status(200).send('hello world');         // Ensure exact response
 });
 
+// Callback function to send success response
+function sendJsonSuccess(req: Request, res: Response) {
+    res.status(200).json({ success: true });
+}
+
+// GET endpoint
+app.get('/task2', sendJsonSuccess);
+
+// POST endpoint
+app.post('/task2', sendJsonSuccess);
+
+// PATCH endpoint
+app.patch('/task2', sendJsonSuccess);
+
+// PUT endpoint
+app.put('/task2', sendJsonSuccess);
+
+// DELETE endpoint
+app.delete('/task2', sendJsonSuccess);
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
