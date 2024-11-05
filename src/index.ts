3005c9a4e8726edc2e92af3ee0556395598bbac8
yasser <yasser.0.yahia@protonmail.com>
2024-11-05 18:17:03 +0200

    task-6

--- src/index.ts
+++ src/index.ts
@@ -39,28 +39,55 @@ const onlyPassAuthorized = (admin_name : string) =>{
 const sendJsonSuccess = (req: Request , res:Response) =>{
     res.status(200).json({success : true})
 }
+// task 1
 
 app.get('/task1' , (req:Request , res:Response) =>{
     res.type("text/plain").status(200).send("hello world")
 })
+
+// task 2
 app.get('/task2', sendJsonSuccess)
 app.post('/task2', sendJsonSuccess)
 app.patch('/task2' , sendJsonSuccess)
 app.put('/task2' ,sendJsonSuccess )
 app.delete('/task2' ,sendJsonSuccess )
 
-
+// task 3
 app.get('/task3' ,onlyPassAuthenticated ,sendJsonSuccess)
-
+// task 4
 
 app.get('/task4' ,[onlyPassAuthenticated,onlyPassAuthorized("ya 3m efta7 ana 3omda")] ,sendJsonSuccess)
 
+// task 5
 
 app.get('/task5/get-admin-name',(req:Request , res:Response)=>{
     res.status(200).json({admin_name : process.env.admin_name})
 })
 
 app.get('/task5/admin-only',[onlyPassAuthenticated,onlyPassAuthorized(process.env.admin_name as string)] ,sendJsonSuccess)
+
+
+
+// task 6
+
+let studentNames : string[] = []
+
+app.get('/task6/students', (req: Request , res:Response)=>{
+    
+    res.status(200).json(studentNames) 
+})
+
+app.post('/task6/students', (req: Request , res:Response)=>{
+    const {name} = req.body
+
+    if(name && typeof(name) === "string"){
+        studentNames.push(name)
+        res.status(200).json({message: `${name} has been added`})
+    }else{
+        res.status(400).json({message: "eb3at al request 3edel m4 na2sa bugs"})
+    }
+})
+
 
 app.listen(port , ()=>{
     console.log("listening on port 3000....")
