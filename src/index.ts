73f4b3d7defe8f32fb4ebb25b76543d08c4b8a60
baraaml <baraalearnsml@gmail.com>
2024-11-07 21:50:07 +0200

    task 3 finished

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import express, { Request, Response } from 'express';
 import cors from "cors";
+import { NextFunction } from 'express-serve-static-core';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+const onlyPassAuthenticated = (req : Request, res : Response, next: NextFunction) =>{ // the middleware
+  if (req.headers.authorization) next();
+  else res.status(401).json({ message: "meeeeeeen?" });
+}
 
 app.use(cors());
 
@@ -15,16 +20,19 @@ app.get('/task1/', (req, res) => {
     res.status(200).contentType('text/plain').send('hello world');
   });
 
-  // Create 5 endpoints: GET, POST, PATCH, PUT, DELETE on the same path /task2
-  function sendJsonSuccess(req: Request, res: Response) {
-    res.status(200).json({ success: true });
-  }
+
+function sendJsonSuccess(req: Request, res: Response, next: NextFunction){ 
+  res.status(200).json({ success: true });
+}
   
 app.get('/task2/', sendJsonSuccess);
 app.post('/task2/', sendJsonSuccess);
 app.patch('/task2/', sendJsonSuccess);
 app.put('/task2/', sendJsonSuccess);
 app.delete('/task2/', sendJsonSuccess);
+
+
+app.get('/task3/', onlyPassAuthenticated, sendJsonSuccess);
 
   app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
