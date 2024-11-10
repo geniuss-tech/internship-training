6f0ef5f791d690a3e91d5da9d584b12157f7e678
Mariam_Elsioffy <mariam.sioffy@gmail.com>
2024-11-10 22:03:42 +0200

    task 5  were added

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 import express, { Request, Response , NextFunction} from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
+
 const app = express();
 const PORT = 3001;
 app.use(cors());
+
+// Access admin_name from environment variables
+const ADMIN_NAME = process.env.admin_name;
 
 // Callback used tasks
 function sendJsonSuccess(req: Request, res: Response) {
@@ -31,6 +39,18 @@ const onlyPassAuthorized = (admin_name: string) => {
   };
 };
 
+// Unprotected route to get the admin name
+app.get('/task5/get-admin-name', (req: Request, res: Response) => {
+  res.status(200).json({ admin_name: ADMIN_NAME });
+ //console.log(ADMIN_NAME);
+});
+
+// Protected route that uses authentication and authorization
+app.get('/task5/admin-only', 
+  onlyPassAuthenticated,
+  onlyPassAuthorized(ADMIN_NAME as string), // Pass admin_name from environment variables
+  sendJsonSuccess
+);
 //task4
 app.get('/task4', 
   onlyPassAuthenticated, 
