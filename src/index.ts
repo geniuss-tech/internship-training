6726104db5463252891ed4314ad108eb89ef25de
baraaml <baraalearnsml@gmail.com>
2024-11-13 07:04:56 +0200

    intializd postgres db, inserted some students

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,41 @@
 import express, { Request, Response } from 'express';
 import cors from "cors";
 import { NextFunction } from 'express-serve-static-core';
-import dotenv from 'dotenv';
-
-// Load environment variables
-dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+const path = require('path');
+const {Pool, Client} = require('pg');
 
 
+// Load environment variables
+
+require('dotenv').config({
+  override: true, 
+  path: path.join(__dirname, 'development.env')
+});
+
+
+const pool = new Pool({
+  user: process.env.USER,
+  host: process.env.HOST,
+  database:process.env.DATABASE,
+  password: process.env.PASSWORD,
+  port: process.env.PORT 
+});
+
+(async () => {
+  const client = await pool.connect();
+  try {
+    const { rows } = await client.query('SELECT current_user');
+    const currentUser = rows[0]['current_user'];
+    console.log(currentUser);
+  } catch (err) {
+    console.error(err);
+  } finally {
+    client.release();
+  }
+})();
 
 app.use(cors());
 app.use(express.json()) // This line enables Express to parse JSON payloads.
